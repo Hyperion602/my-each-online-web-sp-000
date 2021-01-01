ed85496515ab8f34b8bb4028d53feabf64f1c9ab
James Harris <mharris9608@yahoo.com>
2021-01-01 00:31:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,19 +5,3 @@ def my_each(collection)
     i = i + 1
   end
 end
-
-
-def hello_t(array)
-  if block_given?
-    i = 0
-
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
